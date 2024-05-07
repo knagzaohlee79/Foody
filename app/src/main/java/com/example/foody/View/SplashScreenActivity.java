2fcc5dec8007c8ec19f7a94271083118836ee45a
Hoang Anh <anhlhfw@gmail.com>
2024-05-08 03:10:08 +0700

    Dev UI Splash Screen and Login layout

---
+++ app/src/main/java/com/example/foody/View/SplashScreenActivity.java
@@ -0,0 +1,36 @@
+package com.example.foody.View;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.foody.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+TextView txtVersion;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.layout_spashscreen);
+        txtVersion = findViewById(R.id.txtVersion);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            try {
+                PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(),0);
+                txtVersion.setText("Version"+" "+packageInfo.versionName);
+            } catch (PackageManager.NameNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            return insets;
+        });
+    }
+}
