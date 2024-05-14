1588d532fd2d08075d01c5a0eea844d0a4d168ca
Hoang Anh <anhlhfw@gmail.com>
2024-05-14 17:09:14 +0700

    Google Sign-in

--- app/src/main/java/com/example/foody/View/LoginActivity.java
+++ app/src/main/java/com/example/foody/View/LoginActivity.java
@@ -1,17 +1,60 @@
 package com.example.foody.View;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.foody.R;
+import com.facebook.FacebookSdk;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.internal.OnConnectionFailedListener;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements OnConnectionFailedListener, View.OnClickListener {
+     Button btnGoogleSignin;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.layout_login);
 
+        btnGoogleSignin = (Button) findViewById(R.id.btnGoogleSingin);
+        btnGoogleSignin.setOnClickListener(this);
+
+    }
+
+    private void googleSignIn() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder()
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        GoogleApiClient apiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this::onConnectionFailed)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (v.getId() == R.id.btnGoogleSingin) {
+            googleSignIn();
+        }
     }
 }
